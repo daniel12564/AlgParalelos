babf3c3b898e8badc87476a32d804f4676c32170
Your Name <you@example.com>
2015-04-28 06:10:36 -0500

    juego de la vida

---
+++ game_of_life/game.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define tam 10
+#define tam_i tam
+#define tam_j tam
+
+typedef struct My_pair
+{
+	int pos_i;
+	int pos_j;
+} My_pair;
+
+MPI_Datatype createRecType()
+{
+    // Set-up the arguments for the type constructor
+    MPI_Datatype new_type;
+
+    int count = 2;
+    int blocklens[] = { 1,1 };
+
+    MPI_Aint indices[2];
+    indices[0] = (MPI_Aint)offsetof(struct My_pair, pos_i);
+    indices[1] = (MPI_Aint)offsetof(struct My_pair, pos_j);
+
+    MPI_Datatype old_types[] = {MPI_INT,MPI_INT};
+
+    MPI_Type_struct(count,blocklens,indices,old_types,&new_type);
+    // Create a resized type
+    MPI_Datatype resized_new_type;
+    MPI_Type_create_resized(new_type, indices[0], (MPI_Aint)sizeof(struct My_pair), &resized_new_type);
+    MPI_Type_commit(&resized_new_type);
+    // Free new_type as it is no longer needed
+    MPI_Type_free(&new_type);
+
+    return resized_new_type;
+}
+
+char** leer_universo()
+{
+	int  i, j, k;
+    char** universo;
+    universo = (char **) malloc(tam_i * sizeof(char *));
+	for(i = 0; i < tam_i; i++)
+			universo[i] = (char *) malloc(tam_j * sizeof(char));
+	i = 0;
+	j = 0;
+	k = 0;
+	FILE *file;
+	char buffer[100];
+	file = fopen("universo.txt", "rt");
+	while(feof(file) == 0)
+	{
+		fscanf(file, "%s" ,buffer);
+		for(j = 0; j < tam_j; j++)
+		{
+			universo[i][j] = buffer[j];
+			//printf("%c-%c ", buffer[j], universo[i][j]);
+		}
+		//printf("\n");
+		i++;
+		//for(i = 0; i < tam_i; i++)
+			//for(j = 0; j < tam_j; j++)	
+			
+	}
+	fclose(file);
+	return universo;
+}
+
+void print_universo(char** universo)
+{
+	int i, j;
+	for(i = 0; i < tam_i; i++)
+	{
+		for(j = 0; j < tam_j; j++)
+			printf("%c", universo[i][j]); ;
+		printf("\n");
+	}
+}
+
+int modulo(int a, int b)
+{
+	if(a < 0)
+		return (a+b) % b;
+	return a % b;
+}
+
+char evaluar(char** universo, int x, int y)
+{
+	int x_pos = modulo(x + 1, tam_i);
+	int x_neg = modulo(x - 1, tam_i);
+	int y_pos = modulo(y + 1, tam_j);
+	int y_neg = modulo(y - 1, tam_j);
+	int contador = 0;
+	contador += (universo[x_neg][y_neg] == '1') ? 1 : 0;
+	contador += (universo[x_neg][y] == '1') ? 1 : 0;
+	contador += (universo[x_neg][y_pos] == '1') ? 1 : 0;
+	contador += (universo[x][y_neg] == '1') ? 1 : 0;
+	contador += (universo[x][y_pos] == '1') ? 1 : 0;
+	contador += (universo[x_pos][y_neg] == '1') ? 1 : 0;
+	contador += (universo[x_pos][y] == '1') ? 1 : 0;
+	contador += (universo[x_pos][y_pos] == '1') ? 1 : 0;
+	if(contador < 2 || contador > 3)
+		return '0';
+	if(contador == 2 || contador == 3)
+		return '1';
+}
+
+void game_of_life(char** universo, int generaciones)
+{
+	if(generaciones > 0)
+	{
+		printf("Universo en generacion: %d\n", generaciones);
+		print_universo(universo);
+	}
+	else
+		return;
+	int i, j, k;
+	int rank, size;
+	int bloque, n_i, n_j;
+	char** n_universo;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	bloque = tam_i / size;
+	My_pair vector_universo[tam_i * tam_j];
+	My_pair vector_bloque[bloque * tam_j];
+
+	if(!rank)
+	{
+		k = 0;
+		for(i = 0; i < tam_i; i++)
+		{
+			for(j = 0; j < tam_j; j++)
+			{
+				My_pair t;
+				t.pos_i = i;
+				t.pos_j = j;
+				vector_universo[k++] = t;
+			}
+		}
+	}
+	MPI_Datatype MPI_My_pair = createRecType();
+	MPI_Scatter(vector_universo, bloque * tam_j, MPI_My_pair, vector_bloque, bloque * tam_j, MPI_My_pair, 0, MPI_COMM_WORLD);
+
+	for(k = 0; k < size; k++)
+    {
+        if(k == rank)
+        {
+			for(i = 0; i < bloque * tam_j; i++)
+				n_universo[vector_bloque[i].pos_i][vector_bloque[i].pos_j] = evaluar(universo, vector_bloque[i].pos_i, vector_bloque[i].pos_j);
+		}
+	}
+	printf("Nuevo universo\n");
+	print_universo(n_universo);
+
+	if(!rank)
+	{
+		//game_of_life(n_universo, generaciones--);
+	}
+
+}
+
+int main(int argc, char* argv[])
+{
+	int rank, size, generaciones, i;
+	char** universo = leer_universo();
+
+	MPI_Init(&argc, &argv);
+	game_of_life(universo, 3);
+	MPI_Finalize();
+	return 0;
+}
--- game_of_life/life.c
+++ game_of_life/life.c
@@ -2,72 +2,195 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define tam 20
+#define tam 10
+#define tam_i tam
+#define tam_j tam
 
-void Build_mpi_type(bool** universo, int i, int j, MPI_Datatype* tipo)
+char** leer_universo()
 {
-	int array_of_blockslengths[3] = {1, 1, 1};
-	MPI_Datatype array_of_types[3] = {MPI_BOOL, MPI_INT_ MPI_INT};
-	MPI_Aint a_addr, b_addr, c_addr;
-	MPI_Aint array_of_displacements[3] = {0};
-
-	MPI_Get_address(universo, &a_addr);
-	MPI_Get_address(i, &b_addr);
-	MPI_Get_address(j, &c_addr);
-	array_of_displacements[1] = b_addr - a_addr;
-	array_of_displacements[2] = c_addr - a_addr;
-	MPI_Type_create_struct(3, array_of_blockslengths, array_of_displacements, array_of_types, tipo);
+	int  i, j, k;
+    char** universo;
+    universo = (char **) malloc(tam_i * sizeof(char *));
+	for(i = 0; i < tam_i; i++)
+			universo[i] = (char *) malloc(tam_j * sizeof(char));
+	i = 0;
+	j = 0;
+	k = 0;
+	FILE *file;
+	char buffer[100];
+	file = fopen("universo.txt", "rt");
+	while(feof(file) == 0)
+	{
+		fscanf(file, "%s" ,buffer);
+		for(j = 0; j < tam_j; j++)
+		{
+			universo[i][j] = buffer[j];
+			//printf("%c-%c ", buffer[j], universo[i][j]);
+		}
+		//printf("\n");
+		i++;
+		//for(i = 0; i < tam_i; i++)
+			//for(j = 0; j < tam_j; j++)	
+			
+	}
+	fclose(file);
+	return universo;
 }
 
-void set_universo(bool** universo)
+void print_universo(char** universo)
 {
 	int i, j;
-	universo = (bool **) malloc(tam * sizeof(bool *));
-	for(i = 0; i < tam; i++)
-		universo[i] = (bool *) malloc(tam * sizeof(bool));
-	for(i = 0; i < tam; i++)
-		for(j = 0; j < tam; j++)
-			universo[i][j] = (bool) (rand() % 1);
+	for(i = 0; i < tam_i; i++)
+	{
+		for(j = 0; j < tam_j; j++)
+			printf("%c", universo[i][j]); ;
+		printf("\n");
+	}
 }
 
-void game_of_life(bool** universo, int generaciones)
+int modulo(int a, int b)
 {
-	if(!generaciones)
-		return;
+	if(a < 0)
+		return (a+b) % b;
+	return a % b;
+}
 
-	int i, j;
+char evaluar(char** universo, int x, int y)
+{
+	int x_pos = modulo(x + 1, tam_i);
+	int x_neg = modulo(x - 1, tam_i);
+	int y_pos = modulo(y + 1, tam_j);
+	int y_neg = modulo(y - 1, tam_j);
+	int contador = 0;
+	contador += (universo[x_neg][y_neg] == '1') ? 1 : 0;
+	contador += (universo[x_neg][y] == '1') ? 1 : 0;
+	contador += (universo[x_neg][y_pos] == '1') ? 1 : 0;
+	contador += (universo[x][y_neg] == '1') ? 1 : 0;
+	contador += (universo[x][y_pos] == '1') ? 1 : 0;
+	contador += (universo[x_pos][y_neg] == '1') ? 1 : 0;
+	contador += (universo[x_pos][y] == '1') ? 1 : 0;
+	contador += (universo[x_pos][y_pos] == '1') ? 1 : 0;
+	if(contador < 2 || contador > 3)
+		return '0';
+	if(contador == 2 || contador == 3)
+		return '1';
+}
+
+void game_of_life(char** universo, int generaciones)
+{
+	int i, j, k;
 	int rank, size;
+	int bloque, n_i, n_j;
+	char** n_universo;
+	char** tmp;
+
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	bool** n_universo;
-	bool* aux;
-	n_universo = (bool **) malloc(tam * sizeof(bool *));
-	aux = (bool *) malloc(tam * sizeof(bool));
-	for(i = 0; i < tam; i++)
-		n_universo[i] = (bool *) malloc(tam * sizeof(bool));
+	bloque = tam_i / size;
+	
+	char vector_universo[tam_i * tam_j];
+	char vector_bloque[bloque * tam_j];
 
-	for(i = 0; i < tam; i++)
+	if(!rank)
 	{
-		if(!rank)
+		k = 0;
+		for(i = 0; i < tam_i; i++)
 		{
-			MPI_Scatter(void* send_data, int send_count, MPI_Datatype send_datatype, void* recv_data, int recv_count, MPI_Datatype recv_datatype, int root, MPI_Comm communicator);
-		}
-		else
-		{
-
+			for(j = 0; j < tam_j; j++)
+				vector_universo[k++] = universo[i][j];
 		}
 	}
 
-	game_of_life(n_universo, generaciones - 1);
+	MPI_Datatype blocktype;
+    MPI_Datatype blocktype2;
+
+    MPI_Type_vector(bloque, tam_j, tam_j, MPI_CHAR, &blocktype2);
+    MPI_Type_create_resized(blocktype2, 0, sizeof(char), &blocktype);
+    MPI_Type_commit(&blocktype);
+
+    int disps[size];
+    int counts[size];
+    for(i = 0; i < size; i++)
+    {
+        disps[i] = 2* i * tam_j;
+		counts[i] = 1;
+    }
+
+    MPI_Scatterv(vector_universo, counts, disps, blocktype, vector_bloque, bloque * tam_j, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    for(k = 0; k < size; k++)
+    {
+        if(k == rank)
+        {
+            printf("Rank = %d\n", rank);
+            /*if(!rank)
+            {
+                printf("Global matrix: \n");
+                for(i = 0; i < tam_i; i++)
+                {
+                    for(j = 0; j < tam_j; j++)
+                        printf("%c ",vector_universo[i*tam_j+j]);
+                    printf("\n");
+                }
+            }*/
+            printf("Local Matrix:\n");
+            for(i = 0; i < bloque; i++)
+            {
+                for(j = 0; j < tam_j; j++)
+                {
+                	//n_i = (rank+i);
+                	//n_j = j;
+                    printf("%c ",vector_bloque[i*tam_j+j]);
+                    //printf("%c ", universo[n_i][n_j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+
+	/*bloque = tam_i / size;
+	from = rank *  bloque;
+	to = ((rank + 1) *  bloque) - 1;
+
+
+	if(!rank)
+	{
+		n_universo = (char **) malloc(tam_i * sizeof(char *));
+		for(i = 0; i < tam_i; i++)
+			n_universo[i] = (char *) malloc(tam_j * sizeof(char));
+	}
+
+	tmp = (char **) malloc()
+
+	MPI_Scatter(universo, tam_i * tam_j / size, MPI_CHAR, universo[from], tam_i * tam_j / size, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+	for(i = from; i < to; i++)
+	{
+		for(j = 0; j < tam_j; j++)
+		{
+			n_universo[i][j] = evaluar(universo, i, j);
+		}
+	}
+
+	if(!rank)
+	{
+		MPI_Gather(n_universo[from], tam_i * tam_j / size, MPI_CHAR, n_universo, tam_i * tam_j / size, MPI_CHAR, 0, MPI_COMM_WORLD);
+		game_of_life(n_universo, generaciones - 1);
+	}*/
 }
 
 int main(int argc, char* argv[])
 {
-	int rank, size, generaciones, i, j;
+	int rank, size, generaciones, i;
+	char** universo = leer_universo();
+	//print_universo(universo);
+
 	MPI_Init(&argc, &argv);
-	
-	game_of_life(generaciones);
+	game_of_life(universo, generaciones);
 	MPI_Finalize();
 	return 0;
 }
