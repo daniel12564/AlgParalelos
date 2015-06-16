8329528531da498b212dcc31a47699ef4bebb273
Your Name <you@example.com>
2015-06-16 17:36:05 -0500

    mpi+omp

--- mpi-omp/problem.c
+++ mpi-omp/problem.c
@@ -2,23 +2,41 @@
 #include "mpi.h"
 #include <omp.h>
 
-int main(int argc, char *argv[]) {
-  int numprocs, rank, namelen;
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int iam = 0, np = 1;
+int thread_count;
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Get_processor_name(processor_name, &namelen);
+int main(int argc, char *argv[])
+{
+    thread_count = 5;
+    int tam = 40 * thread_count;
 
-  #pragma omp parallel default(shared) private(iam, np)
-  {
-    np = omp_get_num_threads();
-    iam = omp_get_thread_num();
-    printf("Hello from thread %d out of %d from process %d out of %d on %s\n",
-           iam, np, rank, numprocs, processor_name);
-  }
+    int numprocs, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  MPI_Finalize();
+    srand(time(NULL)*rank);
+    int* rand_nums;
+    int local_sum, ii, global_sum;
+    if (!rank)
+    {
+        rand_nums = (int*) malloc(sizeof(int) * tam);
+        int i;
+        for (i = 0; i < tam; i++)
+            rand_nums[i] = rand();
+
+        for (i = 0; i < tam; i++)
+            printf("%d ", rand_nums[i]);
+        printf("\n");
+    }
+    # pragma omp parallel for num_threads(thread_count)\
+    private(ii, local_sum),shared(tam)
+        for(ii=0; ii< tam; ii++)
+            local_sum += rand_nums[ii];
+    printf("suma local %d del proceso %d \n", local_sum, rank);
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (!rank)
+      printf("suma total = %d\n", global_sum);
+
+    MPI_Finalize();
 }
